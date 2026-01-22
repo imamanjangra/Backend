f832a824840b613466b1357270e7f6ad573cf23c
imamanjangra <imamanjangra@gmail.com>
2026-01-22 16:26:38 +0530

    utils code learn

--- Structure/src/index.js
+++ Structure/src/index.js
@@ -1,4 +1,13 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is runing at Prot : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MONGO DB connection failed" , error);
+})
